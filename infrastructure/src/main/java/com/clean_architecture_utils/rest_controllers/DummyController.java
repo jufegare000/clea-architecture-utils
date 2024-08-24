b3207ea9bb145ad98f7db49e4d9d64dcac18cd85
jfgallo <juanfgallo@localhost.localdomain>
2024-08-24 13:59:20 -0500

    hello world implemented successfully

---
+++ infrastructure/src/main/java/com/clean_architecture_utils/rest_controllers/DummyController.java
@@ -0,0 +1,16 @@
+package com.clean_architecture_utils.rest_controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clean-architecture")
+public class DummyController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
---
+++ scripts/requests/check-service.sh
@@ -0,0 +1,2 @@
+curl -X GET http://localhost:8080/api/clean-architecture \
+	-v
