8068ce8706f20bf6c781ee8182b728a0f4d3fb4e
Gerardo Castillo <64170361+GerarC@users.noreply.github.com>
2024-09-24 22:44:50 -0500

    Revert "Some algorithms examples"

--- application/src/main/java/com/clean_architecture_utils/algorithms/searching/binary/module/BinarySearchOperator.java
+++
@@ -1,8 +0,0 @@
-package com.clean_architecture_utils.algorithms.searching.binary.module;
-
-import com.clean_architecture_utils.algorithms.searching.binary.BinarySearchData;
-
-public interface BinarySearchOperator {
-
-    int performBinarySearch(BinarySearchData binarySearchData);
-}
--- application/src/main/java/com/clean_architecture_utils/algorithms/searching/binary/module/BinarySearchPresenter.java
+++
@@ -1,6 +0,0 @@
-package com.clean_architecture_utils.algorithms.searching.binary.module;
-
-public interface BinarySearchPresenter {
-
-    int findElementUsingBinarySearch(int[] arr, int element);
-}
